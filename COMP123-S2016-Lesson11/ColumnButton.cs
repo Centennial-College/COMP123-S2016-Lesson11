ce5ee3ba489f94f901680346b5e08d62c5e2ba11
Kevin Ma <kbma.kevin@gmail.com>
2016-08-22 16:59:42 -0400

    added ColumnButton enum

---
+++ COMP123-S2016-Lesson11/ColumnButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson11
+{
+    public enum ColumnButton
+    {
+        Details = 4,
+        Edit,
+        Delete
+    }
+}
